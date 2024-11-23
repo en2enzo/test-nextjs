a48c3032c36fadaf0b78ec0cf09d4cf42a003fe7
en2enzo <pikaliza@aol.com>
2024-11-24 00:17:05 +0900

    test

---
+++ git-init.sh
@@ -0,0 +1,7 @@
+echo "# test-nextjs" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/en2enzo/test-nextjs.git
+git push -u origin main
